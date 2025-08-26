27f23522c3b1a2eeec29937b3f7fa5ea0624335a
7Semi_tech <tech01@7semi.com>
2025-08-26 17:12:07 +0530

    Update basic.ino

--- examples/basic.ino
+++ examples/basic.ino
@@ -1,1 +1,16 @@
+#include "ACS772_CurrentSensor.h"
+
+ACS772_CurrentSensor sensor(A0);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  float current = sensor.readCurrent();
+  Serial.print("Current: ");
+  Serial.print(current, 3);
+  Serial.println(" A");
+  delay(500);
+}
 
