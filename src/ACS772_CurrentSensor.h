ce48031e058be02874900dcbf8813fed3f6fb424
7Semi_tech <tech01@7semi.com>
2025-08-26 17:33:52 +0530

    Update ACS772_CurrentSensor.h

--- src/ACS772_CurrentSensor.h
+++ src/ACS772_CurrentSensor.h
@@ -1,18 +1,16 @@
-#ifndef ACS772_CurrentSensor_H
-#define ACS772_CurrentSensor_H
+#ifndef ACS772_CURRENTSENSOR_H
+#define ACS772_CURRENTSENSOR_H
 
-#include "Arduino.h"
+#include <Arduino.h>
 
 class ACS772_CurrentSensor {
-public:
-    ACS772_CurrentSensor(uint8_t analogPin, float vcc = 5.0, float zeroCurrentVoltage = 2.5, float sensitivity = 0.04);
-    float readCurrent(uint8_t samples = 10);
-
-private:
+  public:
+    ACS772_CurrentSensor(uint8_t analogPin, float sensitivity = 0.04, float vRef = 2.5);
+    float readCurrent(); // Returns current in Amperes
+  private:
     uint8_t _analogPin;
-    float _vcc;
-    float _zeroCurrentVoltage;
     float _sensitivity;
+    float _vRef;
 };
 
 #endif
