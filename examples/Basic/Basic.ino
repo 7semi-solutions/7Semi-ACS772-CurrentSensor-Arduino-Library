921e1622656ce0e32fb8fc894d6af26b6f7711de
7Semi_tech <tech01@7semi.com>
2025-09-01 16:41:53 +0530

    Update Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,15 +1,22 @@
 #include <ACS772_CurrentSensor.h>
- 
-ACS772_CurrentSensor sensor(A0); // Default 40mV/A sensitivity, 2.5V Vref
- 
+
+// Sensor on A0
+ACS772_CurrentSensor sensor(A0);
+
+// Configurable values
+float sensitivity = 0.04; // 40mV/A
+float vRef = 2.5;         // Reference voltage (depends on supply and calibration)
+float vcc = 5.0;          // ADC reference voltage (use 3.3 for ESP32, STM32, etc.)
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  sensor.begin(sensitivity, vRef, vcc);
 }
- 
+
 void loop() {
   float current = sensor.readCurrent();
   Serial.print("Current: ");
-  Serial.print(current, 3);
+  Serial.print(current, 2);
   Serial.println(" A");
   delay(500);
 }
